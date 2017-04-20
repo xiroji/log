352d92b4c61b0f1b4e344cc36a4d7dc30d1c004b
Hardeep Shoker <hardeep@xiroji.com>
2017-04-19 20:43:08 -0700

    Removing extraneous const & gofmt

--- log.go
+++ log.go
@@ -1,158 +1,154 @@
 package log
 
 import (
-  "io"
-  "os"
-  "fmt"
-  "time"
-  "strings"
-  "encoding/json"
-  "sync"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"sync"
+	"time"
 )
 
 type MixedJSON map[string]interface{}
 
 type Log struct {
-  name string
-  mutex sync.Mutex
-  out io.Writer
-  buffer []byte
-  logLevel int
+	name     string
+	mutex    sync.Mutex
+	out      io.Writer
+	buffer   []byte
+	logLevel int
 }
 
 type Formatted struct {
-  time time.Time
-  hostname string
-  pid int
-  level int
-  fields *MixedJSON
+	time     time.Time
+	hostname string
+	pid      int
+	level    int
+	fields   *MixedJSON
 }
 
-const (
-  )
-
 var LogLevels = map[string]int{
-  "fatal": 60,
-  "error": 50,
-  "warn": 40,
-  "debug": 20,
-  "trace":10}
+	"fatal": 60,
+	"error": 50,
+	"warn":  40,
+	"debug": 20,
+	"trace": 10}
 
 func New(n string) Log {
+	rawEnv, existsLogLevel := os.LookupEnv("LOG_LEVEL")
 
-  rawEnv, existsLogLevel := os.LookupEnv("LOG_LEVEL")
+	logger := Log{
+		name:     n,
+		mutex:    sync.Mutex{},
+		out:      os.Stderr,
+		logLevel: LogLevels["error"]}
 
-  logger :=  Log{
-    name: n,
-    mutex: sync.Mutex{},
-    out: os.Stderr,
-    logLevel: LogLevels["error"]}
+	if existsLogLevel {
+		env, err := LogLevels[strings.ToLower(rawEnv)]
+		if !err {
+			logger.Fatal("Bad LOG_LEVEL")
+			os.Exit(1)
+		}
+		logger.logLevel = env
+	}
 
-  if existsLogLevel {
-    env, err := LogLevels[strings.ToLower(rawEnv)]
-    if (!err)  {
-      logger.Fatal("Bad LOG_LEVEL")
-      os.Exit(1)
-    }
-    logger.logLevel = env
-  }
-
-  return logger
+	return logger
 }
 
 func (l *Log) shouldLog(level int) bool {
-  if (l.logLevel <= level) {
-    return true
-  } else {
-    return false
-  }
+	if l.logLevel <= level {
+		return true
+	} else {
+		return false
+	}
 }
 
 func (l *Log) Output(message string) {
-  l.mutex.Lock()
-  defer l.mutex.Unlock()
-  l.buffer = l.buffer[:0]
-  l.buffer = append(l.buffer, message...)
-  l.out.Write(l.buffer)
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+	l.buffer = l.buffer[:0]
+	l.buffer = append(l.buffer, message...)
+	l.out.Write(l.buffer)
 }
 
 func (l *Log) Fatal(message interface{}) {
-  l.Fatalf(nil, message)
+	l.Fatalf(nil, message)
 }
 
 func (l *Log) Error(message interface{}) {
-  l.Errorf(nil, message)
+	l.Errorf(nil, message)
 }
 
 func (l *Log) Warn(message interface{}) {
-  l.Warnf(nil, message)
+	l.Warnf(nil, message)
 }
 
 func (l *Log) Debug(message interface{}) {
-  l.Debugf(nil, message)
+	l.Debugf(nil, message)
 }
 
 func (l *Log) Trace(message interface{}) {
-  l.Tracef(nil, message)
+	l.Tracef(nil, message)
 }
 
 func (l *Log) Fatalf(fields MixedJSON, message interface{}) {
-  level := LogLevels["fatal"]
-  if (!l.shouldLog(level)) {
-    return
-  }
-  l.Output(l.toString(level, &fields, message))
-  os.Exit(1)
+	level := LogLevels["fatal"]
+	if !l.shouldLog(level) {
+		return
+	}
+	l.Output(l.toString(level, &fields, message))
+	os.Exit(1)
 }
 
 func (l *Log) Errorf(fields MixedJSON, message interface{}) {
-  level := LogLevels["error"]
-  if (!l.shouldLog(level)) {
-    return
-  }
-  l.Output(l.toString(level, &fields, message))
+	level := LogLevels["error"]
+	if !l.shouldLog(level) {
+		return
+	}
+	l.Output(l.toString(level, &fields, message))
 }
 
 func (l *Log) Warnf(fields MixedJSON, message interface{}) {
-  level := LogLevels["warn"]
-  if (!l.shouldLog(level)) {
-    return
-  }
-  l.Output(l.toString(level, &fields, message))
+	level := LogLevels["warn"]
+	if !l.shouldLog(level) {
+		return
+	}
+	l.Output(l.toString(level, &fields, message))
 }
 
 func (l *Log) Debugf(fields MixedJSON, message interface{}) {
-  level := LogLevels["debug"]
-  if (!l.shouldLog(level)) {
-    return
-  }
-  l.Output(l.toString(level, &fields, message))
+	level := LogLevels["debug"]
+	if !l.shouldLog(level) {
+		return
+	}
+	l.Output(l.toString(level, &fields, message))
 }
 
 func (l *Log) Tracef(fields MixedJSON, message interface{}) {
-  level := LogLevels["trace"]
-  if (!l.shouldLog(level)) {
-    return
-  }
-  l.Output(l.toString(level, &fields, message))
+	level := LogLevels["trace"]
+	if !l.shouldLog(level) {
+		return
+	}
+	l.Output(l.toString(level, &fields, message))
 }
 
 func (l *Log) toString(level int, fields *MixedJSON, message interface{}) string {
-  formatted := l.format(level, fields, message)
-  encoded, _ := json.Marshal(formatted)
-  return fmt.Sprintf("%s\n", encoded)
+	formatted := l.format(level, fields, message)
+	encoded, _ := json.Marshal(formatted)
+	return fmt.Sprintf("%s\n", encoded)
 }
 
 func (l *Log) format(level int, fields *MixedJSON, message interface{}) *MixedJSON {
-  hostname, _ := os.Hostname()
-  formatted := &MixedJSON{
-    "time": time.Now(),
-    "hostname": hostname,
-    "pid": os.Getpid(),
-    "level": level,
-    "fields": &fields,
-    "v": 1,
-    "name": l.name,
-    "msg": message}
-  return formatted
+	hostname, _ := os.Hostname()
+	formatted := &MixedJSON{
+		"time":     time.Now(),
+		"hostname": hostname,
+		"pid":      os.Getpid(),
+		"level":    level,
+		"fields":   &fields,
+		"v":        1,
+		"name":     l.name,
+		"msg":      message}
+	return formatted
 }
